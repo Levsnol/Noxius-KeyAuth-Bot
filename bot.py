9cf99d2710e4b3ad28e3fa2da50e20ab9e2f7ad9
Levsnol <124164978+Levsnol@users.noreply.github.com>
2024-07-13 00:49:11 -0500

    🦇

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import random
 
 import string
 from datetime import datetime
-footerall="t.me/opium | developed by rotomicora"
+footerall="running in background..."
 import discord
 import fade
 import requests
